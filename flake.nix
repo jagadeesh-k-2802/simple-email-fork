00e1cb8513c22690406b29ee8ec50b122e96fd66
Distopico <distopico@riseup.net>
2023-07-23 14:56:49 -0500

    chore: add flake until guix support kotlin

---
+++ flake.nix
@@ -0,0 +1,32 @@
+{
+  description = "Kotlin development environment";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+
+  outputs = { self, nixpkgs }:
+    let
+      javaVersion = 11;
+
+      overlays = [
+        (final: prev: rec {
+          jdk = prev."jdk${toString javaVersion}";
+          gradle = prev.gradle.override { java = jdk; };
+          kotlin = prev.kotlin.override { jre = jdk; };
+        })
+      ];
+      supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+      forEachSupportedSystem = f: nixpkgs.lib.genAttrs supportedSystems (system: f {
+        pkgs = import nixpkgs { inherit system; };
+      });
+    in
+    {
+      devShells = forEachSupportedSystem ({ pkgs }: {
+        default = pkgs.mkShell {
+            packages = with pkgs; [ jdk11 kotlin gradle kotlin-language-server gcc gcc-unwrapped.lib glibc ncurses patchelf zlib ];
+            shellHook = ''
+               export LD_LIBRARY_PATH=${pkgs.gcc-unwrapped.lib}/lib:$LD_LIBRARY_PATH
+            '';
+        };
+      });
+    };
+}
